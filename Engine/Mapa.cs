9d7ca4bcce32405e3d52529adc226ef5475d0e41
GoliasVictor <Dev.jvsb@gmail.com>
2021-11-01 12:24:33 -0300

    Corresão merge
    Por causa de alguns problemas com merge, ao inves de fazelo simplismente copiei do outro branche e colei aqui.

---
+++ Engine/Mapa.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using static Engine.Helper;
+using System.Collections.Concurrent;
+
+namespace Engine
+{
+    [Serializable] 
+    public class Mapa
+    {
+
+        Stopwatch St = new Stopwatch();
+        public readonly HashSet<IBloco> Blocos = new HashSet<IBloco>();   
+        public readonly (double x, double y) Tamanho;
+        public Cord Spawn { get; set; } = (1, 1);
+        public  double Esquerda => -Tamanho.x/2;
+        public  double Direita => Tamanho.x/2;
+        public  double Cima => -Tamanho.y / 2;
+        public  double Baixo => Tamanho.y / 2;
+        public Mapa(int TamX, int TamY)
+        {
+            Tamanho =  (TamX - Bloco.TamanhoPadrao, TamY - Bloco.TamanhoPadrao);
+            var Grossura = Bloco.TamanhoPadrao;
+            Blocos.Add(new Parede(new Cord(Esquerda-Grossura/2, 0), Grossura, TamY + Grossura ));
+            Blocos.Add(new Parede(new Cord(Direita + Grossura / 2, 0), Grossura, TamY + Grossura ));
+            Blocos.Add(new Parede(new Cord(0,Cima - Grossura / 2), TamX + Grossura , Grossura));
+            Blocos.Add(new Parede(new Cord(0,Baixo + Grossura / 2), TamX + Grossura, Grossura));
+        }
+        public void MorteParticula(Particula particula){
+            Blocos.Remove(particula);
+        }
+        public void AdicionarBloco(IBloco bloco)
+        {
+            Blocos.Add(bloco);
+        }
+        public void AdicionarBlocos(IEnumerable<IBloco> blocos)
+        {
+            foreach (IBloco Bloco in blocos)
+                Blocos.Add(Bloco);
+        }
+        static bool Colidindo(IBloco A, IBloco B)
+        {
+            return A.ProximaDireita > B.ProximaEsquerda && A.ProximaCima < B.ProximaBaixo
+                && B.ProximaDireita > A.ProximaEsquerda && B.ProximaCima < A.ProximaBaixo;
+        } 
+        static void Colidir(IBloco A, IBloco B)
+        {  
+            B.Colidir(A);
+            A.Colidir(B);
+        }
+        #region ColisaoParalela 
+        static void ColidirParalelo(IEnumerable<IBloco> blocos){
+            var Sublists = blocos.Select((B) => new List<IBloco>(){B});
+            ColidirEJuntar(new ConcurrentStack<List<IBloco>>(Sublists));
+        }
+        static ConcurrentStack<List<IBloco>> ColidirEJuntar(ConcurrentStack<List<IBloco>> StackListsBloco){
+            var ListasUnidas =  new ConcurrentStack<List<IBloco>>();
+            List<Task> Tasks =  new List<Task>();
+            while(StackListsBloco.Count > 1){
+                StackListsBloco.TryPop(out var A);
+                StackListsBloco.TryPop(out var B);
+                Tasks.Add( Task.Run(()=>{
+                    ListasUnidas.Push(ColidirEJuntarLists(A,B));
+                }));
+            }
+            if(StackListsBloco.Count == 1){
+                StackListsBloco.TryPop(out var Lista);
+                ListasUnidas.Push(Lista);
+            }
+            Task.WhenAll(Tasks).Wait();
+            return ListasUnidas.Count < 2 ? ListasUnidas : ColidirEJuntar(ListasUnidas);
+        }
+        static List<IBloco> ColidirEJuntarLists(List<IBloco> Blocos1,List<IBloco> Blocos2){
+            if(Blocos2 is null)
+                return Blocos1;
+            if(Blocos1 is null)
+                return Blocos2;
+            Parallel.ForEach(Blocos1,(A)=>{
+                foreach (var B in Blocos2)
+                    if(Colidindo(A, B))
+                        Colidir(A, B);
+            });            
+            return Blocos1.Union(Blocos2).ToList();
+        }
+        #endregion
+        static void Colidir(IEnumerable<IBloco> blocos)
+        {
+            if (blocos is null)
+                throw new ArgumentNullException(nameof(blocos));
+            var Blocos = new Stack<IBloco>(blocos);
+            while(Blocos.Count > 0)
+            {
+                var A = Blocos.Pop();
+                foreach (var B in Blocos)
+                    if(Colidindo(A, B))
+                        Colidir(A, B);
+            }
+        }
+        readonly double Steps = 2;
+        public void AtualizarMapa(double VelocidadeTempo = 1)
+        {
+            
+            var TotalSteps = Steps * Math.Abs(VelocidadeTempo);
+            var DeltaT = 1 /(Steps * VelocidadeTempo);
+            St.Start();
+            for (int T = 0; T < TotalSteps; T++)
+            {
+                Tempo += DeltaT;
+                Colidir(Blocos);
+                Parallel.ForEach(  BlocosAtualizaveis, 
+                    (BlocoAtualizavel) =>  BlocoAtualizavel.Atualizar() 
+                );
+            }
+            St.Stop();
+            LogTicks(St.ElapsedTicks);
+            St.Restart();
+        }   
+        List<double> ListTicks = new List<double>();
+        void LogTicks(double Ticks){
+            
+            ListTicks.Add(Ticks);
+            var Media = ListTicks.Sum((T) => T/ListTicks.Count());
+            Console.WriteLine($"{Math.Log2(Media):000000000} ~ {Math.Log2(Ticks):000000000}");
+        }
+        private void MostrarVelocidade(){
+            Vetor VelocidadeTotal = default;
+            foreach (IMovel movel in BlocosMoveis)
+                VelocidadeTotal += movel.Velocidade;
+            Console.WriteLine(VelocidadeTotal + ParedeVelocidade);
+        }
+        private IEnumerable<IAtualizavel> BlocosAtualizaveis => Blocos.OfType<IAtualizavel>();
+		private IEnumerable<IMovel> BlocosMoveis => Blocos.OfType<IMovel>();
+	} 
+}
--- Jogo/Program.cs
+++ Jogo/Program.cs
@@ -1,139 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-using Armazenamento;
-using Biblioteca;
-
-
-using OpenTK;
-using OpenTK.Graphics;
-using OpenTK.Graphics.OpenGL;
-using OpenTK.Input;
 namespace Jogo
 {
-
-    public class Jogador : IJogador
+    public static class Program
     {
-        public EstiloBloco Estilo { get => Tangivel ? new EstiloBloco(Color.Blue) : new EstiloBloco(Color.White); set {; } }
-        public bool Tangivel => tangivel;
-        public bool tangivel { get; set; }
-        public Cord Pos { get; set; }
-        public Cord ProximaPos { get; set; }
-        public Mapa Map { get; set; }
-        public Jogador(Mapa map) { Map = map; }
-        public int DanoMax => 3;
-        public int DanoRecebido{ get;protected set;}
-        public void Dano(int n) 
+        [STAThread]
+        static void Main()
         {
-            if ((DanoRecebido += n) >= DanoMax)
-            {
-                ProximaPos = Map.Spawn.GetCord();
-                DanoRecebido = 0;
-            }
-        }
-        
-        public  void Movimento()
-        {
-            if (Jogo.EstadoTeclado[Key.Up] || Jogo.EstadoTeclado[Key.Down] || Jogo.EstadoTeclado[Key.Left] || Jogo.EstadoTeclado[Key.Right] || Jogo.EstadoTeclado[Key.Q])
-            {
-                
-                if (Jogo.EstadoTeclado[Key.Q] && Jogo.EstadoTeclado[Key.Q] != Jogo.EstadoAnteriorTeclado[Key.Q])
-                    tangivel = !tangivel;
-                ProximaPos = Pos.GetCord();
-                if (Jogo.EstadoTeclado[Key.Up]) ProximaPos.y -= 1;
-                else if (Jogo.EstadoTeclado[Key.Down]) ProximaPos.y += 1;
-                else if (Jogo.EstadoTeclado[Key.Left]) ProximaPos.x -= 1;
-                else if (Jogo.EstadoTeclado[Key.Right]) ProximaPos.x += 1;
-
-                if (tangivel)
-                {
-                    Map.Interagir(this);
-                    if (Map.PosTangivel(ProximaPos))
-                        ProximaPos = Pos.GetCord();
-                }
-
-            }
-        }
-
-        public void AtualizarPos() => Pos = ProximaPos.GetCord();
-
-    }
-    public class Jogo : GameWindow
-    {
-
-        public Mapa Map { get; set; }
-        public static KeyboardState EstadoTeclado;
-        public static KeyboardState EstadoAnteriorTeclado;
-
-        public Jogo(int TamX, int TamY) : base()
-        {
-            Map = MapasPrefeitos.GerarMapaDeTeste1();
-            player = new Jogador(Map);
-            player.Pos = Map.Spawn.GetCord();
-            player.ProximaPos = Map.Spawn.GetCord();
-            Map.Especiais.Insert(0, player);
-
-
-        }
-
-        public Jogador player ;
-        protected override void OnUpdateFrame(FrameEventArgs e)
-        {
-             EstadoTeclado = Keyboard.GetState();
-            foreach (var pixelMovel in Map.Especiais)
-                pixelMovel.Movimento();
-            foreach (var pixelMovel in Map.Especiais)
-                pixelMovel.AtualizarPos();
-            if (Keyboard.GetState()[Key.Escape]) Exit();
-            EstadoAnteriorTeclado = EstadoTeclado;
-
-        }
-        public float ax = 0, ay = 0;
-        protected override void OnLoad(EventArgs e)
-        {
-            base.OnLoad(e);
-            ClientSize = new Size(Map.Tamanho.x * Tela.TamPadrao, +Map.Tamanho.y * Tela.TamPadrao);
-
-        }
-        
-        protected override void OnRenderFrame(FrameEventArgs e) 
-        {
-            base.OnRenderFrame(e);
-            GL.Viewport(0, 0, ClientSize.Width, ClientSize.Height);
-            Matrix4 projection = Matrix4.CreateOrthographic(ClientSize.Width, ClientSize.Height, 0.0f, 1.0f) * Matrix4.CreateTranslation(-1f, +1f, 0);
-            GL.MatrixMode(MatrixMode.Projection);
-            GL.LoadMatrix(ref projection);
-            GL.Clear(ClearBufferMask.ColorBufferBit);
-
-            for (int x = 0; x < Map.Tamanho.x; x++)
-                for (int y = 0; y < Map.Tamanho.y; y++)
-                    Tela.DesenharRetangulo(x, y, Map[x, y].Estilo.Cor);
-            
-            foreach(var pixelMovel in Map.Especiais )            
-                Tela.DesenharRetangulo(pixelMovel);
-
-
-            SwapBuffers();
-        }
-
-    }
-
-    internal class Program
-    {
-        private static void Main()
-        {
-            Console.Write(System.IO.Directory.GetCurrentDirectory());
-            using (var game = new Jogo(21, 21))
-            {
-                game.VSync = VSyncMode.Adaptive;
-                Tela.window = game;
-                game.WindowBorder = WindowBorder.Fixed;
-                game.Run(10.0, 0.0);
-            }
+            using (var game = new Game1())
+                game.Run();
         }
     }
 }
